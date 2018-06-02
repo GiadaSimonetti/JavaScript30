0a2707d9f20396a6e183786701c9c912df448d67
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-02 15:36:20 +0100

    Toggle video player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,20 @@
+// get our elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay(){
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// build our function
+
+// hook up the event listener
