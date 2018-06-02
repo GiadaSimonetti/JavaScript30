5b88891f2324e40b4aea5c31cbb36df25ad2e0e9
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-02 16:07:43 +0100

    Make skip buttons work

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -19,9 +19,16 @@ function updateButton() {
   console.log('update the button');
 }
 
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // hook up the event listener
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
